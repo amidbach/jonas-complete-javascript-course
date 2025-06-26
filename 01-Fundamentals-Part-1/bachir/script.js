ba23c3fa04c14b1f30ab7ee3adbba0a749e9a47a
Bachir A. Bachir (BCX) <amidbach@hotmail.com>
2025-06-26 16:13:28 +0200

    Section 2 - [13] :: Basic Operators - // Math operators

--- 01-Fundamentals-Part-1/bachir/script.js
+++ 01-Fundamentals-Part-1/bachir/script.js
@@ -51,7 +51,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 ////////////////////////////////////
 // let, const and var
@@ -67,3 +66,19 @@ job = 'teacher'
 
 lastName = 'Schmedtmann';
 console.log(lastName);
+*/
+
+////////////////////////////////////
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
