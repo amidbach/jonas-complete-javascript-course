2510a00ce09b618151476a88a789a776e59d6a63
Bachir A. Bachir (BCX) <amidbach@hotmail.com>
2025-06-26 15:31:24 +0200

    Section 2 - [11] :: Data Types

--- 01-Fundamentals-Part-1/bachir/script.js
+++ 01-Fundamentals-Part-1/bachir/script.js
@@ -24,7 +24,29 @@ let PI = 3.1415;
 let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+let job1 = 'programmer';
+let job2 = 'teacher';
 
 console.log(myFirstJob);
+
+////////////////////////////////////
+// {{2}} - [11] :: Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
